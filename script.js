98dd65fca93adfa2397571f1e1ef8b1599d77b6c
revathi <arevathi125@gmail.com>
2024-03-11 16:46:14 -0500

    Made some changes

--- script.js
+++ script.js
@@ -4,10 +4,12 @@
 
 // Write your JavaScript code here!
 
+//const helper = require("./scriptHelper");
+
 window.addEventListener("load", function() {
    // let document = document;
-    let form = document.querySelector("launchForm");
-    form.addEventListener("formSubmit",event =>{
+    let form = document.querySelector("form");
+    form.addEventListener("submit",event =>{
         event.preventDefault();
         
         let list = document.getElementById("faultyItems");
@@ -16,11 +18,13 @@ window.addEventListener("load", function() {
         let fuelLevel = document.querySelector("input[name=fuelLevel]").value;
         let cargoLevel = document.querySelector("input[name=cargoMass]").value;
         formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
+        //helper. formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel);
        });
 
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse = myFetch();
+    //let listedPlanetsResponse = helper.myFetch();
     listedPlanetsResponse.then(function (result) {
         listedPlanets = result;
         //console.log(listedPlanets);
@@ -28,6 +32,7 @@ window.addEventListener("load", function() {
         //console.log(listedPlanets);
         // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        let myrandomPlanet = pickPlanet(listedPlanets);
+       //let myrandomPlanet = helper.pickPlanet(listedPlanets);
        //console.log(myrandomPlanet);
        
        let name = myrandomPlanet.name;
@@ -38,7 +43,8 @@ window.addEventListener("load", function() {
        let imageUrl = myrandomPlanet.image;
       
     //let { name, diameter, star, distance, moons, image } = myrandomPlanet;
-      addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+       addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
+    //helper. addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl);
       })
     
  });
